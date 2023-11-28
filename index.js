a0258521138f5b0f75ae160f61274b41d61eeb38
Richman <richmanong4@gmail.com>
2023-11-27 23:46:49 -1000

    worked on the logic behind the calculator and fixed the css to fit the container

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function divide(x , y) {
     }
 }
 
-let first = '';
-let second = '';
-let operatorion = '';
+let first = null;
+let second = null;
+let operation = null;
 
 function operate(firstNum, operator, secondNum) {
     if (operator === "+") {
@@ -34,3 +34,84 @@ function operate(firstNum, operator, secondNum) {
     }
 }
 
+let displayNum = document.querySelector(".display");
+
+function populate() {
+    const button = document.querySelectorAll(".buttons");
+    button.forEach(e => {
+        e.addEventListener('click', e => {
+            if (e.target.value === "0") {
+                displayNum.textContent = 0;
+                let first = 0;
+            } else if (e.target.value === "1") {
+                displayNum.textContent = 1;
+                let first = 0;
+            } else if (e.target.value === "2") {
+                displayNum.textContent = 2;
+                let first = 0;
+            } else if (e.target.value === "3") {
+                displayNum.textContent = 3;
+                let first = 0;
+            } else if (e.target.value === "4") {
+                displayNum.textContent = 4;
+                let first = 0;
+            } else if (e.target.value === "5") {
+                displayNum.textContent = 5;
+                let first = 0;
+            } else if (e.target.value === "6") {
+                displayNum.textContent = 6;
+                let first = 0;
+            } else if (e.target.value === "7") {
+                displayNum.textContent = 7;
+                let first = 0;
+            } else if (e.target.value === "8") {
+                displayNum.textContent = 8;
+                let first = 0;
+            } else if (e.target.value === "9") {
+                displayNum.textContent = 9;
+                let first = 0;
+            } else if (e.target.class === "clear") {
+                displayNum.className = "clear";
+                operation = "";
+            } else if (e.target.class === "sign") {
+                displayNum.className = "sign";
+                operation = "";
+            } else if (e.target.class === "percent") {
+                displayNum.className = "percent";
+                operation = "%";
+            } else if (e.target.class === "divide") {
+                displayNum.className = "divide";
+                operation = "/";
+            } else if (e.target.class === "multiply") {
+                displayNum.className = "multiply";
+                operation = "*";
+            } else if (e.target.class === "subtract") {
+                displayNum.className = "subtract";
+                operation = "-";
+            } else if (e.target.class === "add") {
+                displayNum.className = "add";
+                operation = "+";
+            } else if (e.target.class === "decimal") {
+                displayNum.className = "decimal";
+                operation = ".";
+            } else if (e.target.class === "cequals") {
+                displayNum.className = "equals";
+                operation = "=";
+            }
+        })
+    });
+    let value = first + second;
+    second = first
+    first = value;
+    check(first, operation, second);
+}
+
+function check(checkFirst, checkOperator, checkSecond) {
+
+}
+
+populate();
+
+console.log(first);
+console.log(second);
+console.log(operation);
