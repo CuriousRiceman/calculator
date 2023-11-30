954b2d33964fd1ab7968717d9246c93501b2a3bc
Richman <richmanong4@gmail.com>
2023-11-30 00:39:05 -1000

    fixed a lot of issues and styling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function divide(x , y) {
     }
 }
 
-let currentInput = '0';
+let currentInput = "0";
 let previousInput = null;
 let currentOperation = null;
 const display = document.querySelector('.display');
@@ -39,7 +39,18 @@ document.querySelector('.equals').addEventListener('click', calculateResult);
 document.querySelector('.clear').addEventListener('click', clearCalculator);
 
 function handleNumberInput(number) {
-    if (currentInput === '0') {
+   if (number === "del") {
+        let stringToArray = display.textContent.split("");
+        if (stringToArray.length > 1) {
+            let slicedInputAndConcat = stringToArray.slice(0, -1).join("");
+            currentInput = slicedInputAndConcat;
+        } //MUST FIX - example is 48 + 12 - 60, del 0 so its 6,
+            //but add a num and equ doesnt work
+    } else if (number === "del") {
+        currentInput = "0";
+    } else if (currentInput === "0" && number === ".") {
+        currentInput += number;
+    } else if (currentInput === "0") {
         currentInput = number;
     } else {
         currentInput += number;
@@ -74,12 +85,14 @@ function calculateResult() {
             case '/':
                 currentInput = num2 !== 0 ? num1 / num2 : 'Error';
                 break;
+            case '**':
+                currentInput = num1 ** num2;
+                break;
         }
     }
-
-    currentOperation = null;
-    previousInput = null;
     updateDisplay();
+    currentInput = null;
+    previousInput = null;
 }
 
 function clearCalculator() {
